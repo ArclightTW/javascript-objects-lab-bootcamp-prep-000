3b946da4911a12046a2a0bfface240b9b68c8976
Tom Wright <tomawright95@gmail.com>
2018-12-30 16:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign(recipes, object);
+  Object.assign(recipes, object)
   var newObj = Object.assign(object)
   delete newObj[key]
   return newObj
