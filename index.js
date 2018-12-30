db315056e4672c22f24d4e8f406e9d00e2a05b30
Tom Wright <tomawright95@gmail.com>
2018-12-30 16:08:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object()
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign(recipes, object);
-  recipes[key] = value;
-  return recipes;
+  Object.assign(recipes, object)
+  recipes[key] = value
+  return recipes
 }
